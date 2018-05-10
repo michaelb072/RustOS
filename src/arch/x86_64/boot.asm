648527a8d56226ef1e9ec7739a6dbf517cd241c8
Michael <michaelb072@gmail.com>
2018-05-09 21:44:49 -0700

    Enabled paging

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -10,6 +10,9 @@ start:
     call check_cpuid
     call check_long_mode
 
+    call set_up_page_tables
+    call enable_paging
+
     ; print 'ok' to screen
     mov dword [0xb8000], 0x2f4b2f4f
     hlt
@@ -27,6 +30,7 @@ check_long_mode:
     test edx, 1 << 29      ; test if the LM-bit is set in the D-register
     jz .no_long_mode       ; If it's not set, there is no long mode
     ret
+
 .no_long_mode:
     mov al, "2"
     jmp error
@@ -76,6 +80,56 @@ check_cpuid:
     mov al, "1"
     jmp error
 
+set_up_page_tables:
+    ; map first p4 entry to p3 table
+    mov eax, p3_table
+    or eax, 0b11 ; present + writable
+    mov [p4_table], eax
+
+    ; map first p3 entry to p2 table
+    mov eax, p2_table
+    or eax, 0b11 ; present + writable
+    mov [p3_table], eax
+
+    ; map each P2 entry to a huge 2MiB page
+    mov ecx, 0
+
+.map_p2_table:
+    ; map ecx-th P2 entry to a huge page that starts with address 2MiB*ecx
+    mov eax, 0x200000  ; 2MiB
+    mul ecx            ; start address of ecx-th page
+    or eax, 0b10000011 ; present + writable + huge
+    mov [p2_table + ecx * 8], eax ; map ecx-th entry
+
+    inc ecx            ; increase counter
+    cmp ecx, 512       ; if counter == 512, the whole p2 table is mapped
+    jne .map_p2_table  ; else map the next entry
+
+    ret
+
+enable_paging:
+    ; load p4 to cr3 register (cpu uses this to access the p4 table)
+    mov eax, p4_table
+    mov cr3, eax
+
+    ; enable PAE-flag in cr4 (Physical Address Extension)
+    mov eax, cr4
+    or eax, 1 << 5
+    mov cr4, eax
+
+    ; set the long mode bit in the EFER MSR (model specific register)
+    mov ecx, 0xC0000080
+    rdmsr
+    or eax, 1 << 8
+    wrmsr
+
+    ; enable paging in the cr0 register
+    mov eax, cr0
+    or eax, 1 << 31
+    mov cr0, eax
+
+    ret
+
 ; Prints 'ERR: ' and the given error code to screen and hangs.
 ; parameter: error code (in ascii) in al
 error:
@@ -86,6 +140,13 @@ error:
     hlt
 
 section .bss
+align 4096
+p4_table:
+    resb 4096
+p3_table:
+    resb 4096
+p2_table:
+    resb 4096
 stack_bottom:
     resb 64
 stack_top:
