a9d59d430fef72b83f44db400008bc12b5a0227d
Michael <michaelb072@gmail.com>
2018-05-13 00:31:52 -0700

    Added vga print interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,20 @@
 #![feature(lang_items)]
+#![feature(unique)]
+#![feature(ptr_internals)]
+#![feature(const_fn)]
 #![no_std]
+
+#[macro_use]
+mod vga_buffer;
+
+extern crate volatile;
+extern crate spin;
 
 #[no_mangle]
 pub extern fn rust_main() {
-    let hello = b"Michael <3 Lele";
-    let color_byte = 0x1f; // white foreground blue background
-
-    // let mut hello_colord = [color_byte; 24];
-    let mut hello_colord = [color_byte; 40];
-
-    for (i, char_byte) in hello.into_iter().enumerate() {
-        hello_colord[i*2] = *char_byte;
-    }
-
-    let buffer_ptr = (0xb8000 + 1988) as *mut _;
-    unsafe { *buffer_ptr = hello_colord };
-
+    vga_buffer::clear_screen();
+    println!("Hello world");
+    println!("{}", { println!("Inner"); "outer" });
     loop{}
 }
 
